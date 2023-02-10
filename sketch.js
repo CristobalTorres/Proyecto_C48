1414f3070fbe1488450ae5054812113a7e2b732f
CristobalTorres <cristobal.torres6771@gmail.com>
2023-02-09 20:28:03 -0500

    archivos

---
+++ sketch.js
@@ -0,0 +1,171 @@
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg, backgrounImg;
+
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+
+var life =3;
+var score=0;
+var gameState=1
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  //reto 1
+  //cambio de imagen
+ blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
+  //adicion personal
+  backgroundImg=loadImage("backgrounf.jpg")
+}
+function setup() {
+  createCanvas(800, 600);
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
+  //adicion pesronal
+  Name = createElement("h3")
+  NameGame=createElement("h2")
+}
+
+function draw() {
+  //adicion personal
+  background(backgroundImg);
+  
+  heading.html("Vida: "+life)
+  heading.style('color:red'); 
+  heading.position(150,20)
+
+  scoreboard.html("Puntuación: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-220,20)
+
+//adicion personal
+  Name.position(600,550);
+  Name.html("Cristobal Torres");
+  Name.style('color:#ffe15a'); 
+
+  NameGame.position(350,10);
+  NameGame.html("Tiro al Blanco");
+  NameGame.style('color:black'); 
+
+  if(gameState===1){
+    gun.y=mouseY  
+
+    if (frameCount % 80 === 0) {
+      drawblueBubble();
+    }
+
+    if (frameCount % 100 === 0) {
+      drawredBubble();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+    if (blueBubbleGroup.collide(backBoard)){
+      handleGameover(blueBubbleGroup);
+    }
+    if (redBubbleGroup.collide(backBoard)) {
+      handleGameover(redBubbleGroup);
+    }
+    
+    if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }
+
+    if(redBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(redBubbleGroup);
+    }
+
+    drawSprites();
+  }
+    
+  
+}
+
+function drawblueBubble(){
+  bluebubble = createSprite(800,random(20,650),40,40);
+  bluebubble.addImage(blueBubbleImg);
+  bluebubble.scale = 0.1;
+  bluebubble.velocityX = -8;
+  bluebubble.lifetime = 400;
+  blueBubbleGroup.add(bluebubble);
+}
+function drawredBubble(){
+  redbubble = createSprite(800,random(20,650),40,40);
+  redbubble.addImage(redBubbleImg);
+  redbubble.scale = 0.1;
+  redbubble.velocityX = -8;
+  redbubble.lifetime = 400;
+  redBubbleGroup.add(redbubble);
+}
+
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
+}
+
+function handleBubbleCollision(bubbleGroup){
+    if (life > 0) {
+       score=score+1;
+    }
+//reto 2
+   blast= createSprite(bullet.x+60, bullet.y, 50,50);
+  blast.addImage(blastImg)
+   blast.scale=0.22
+ blast.life=20
+    bulletGroup.destroyEach()
+    bubbleGroup.destroyEach()
+}
+
+function handleGameover(bubbleGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
+
+    if (life === 0) {
+      gameState=2;
+      GameOver();
+     
+    }
+  
+}
+
+//adicion personal
+function GameOver(){
+  gameOver = createElement("h1")
+  message =createElement("h3")
+
+  gameOver.html("OH NO, PERDISTE")
+  message.html("NO APUNTASTE A TODOS TUS OJETIVOS")
+
+  gameOver.style('color:red')
+  message.style('color:black')
+
+  gameOver.position(250,150)
+  message.position(200,200)
+  
+
+}
